7c5ba217f13a0b4159af285c213ef915da69d9ac
Troels H. Jensen <lord_ni@hotmail.com>
2017-04-21 14:07:02 +0200

    second commit

--- js/LessonsScript.js
+++ js/LessonsScript.js
@@ -1,13 +1,23 @@
 // Global variable for the json data recives from google sheet
-var jsondata;
+var datajson;
 
 // For global use of div id data
-var divElementData = "";
+var data = "";
 
 // ID of the Google Spreadsheet
-var spreadsheetID = "1GLMhpBRmnZSzTMQWHe4RLT25_cOLHiqtu12xLeAxDyU";
+var lessonSpreadsheetID = "1GLMhpBRmnZSzTMQWHe4RLT25_cOLHiqtu12xLeAxDyU";
 
 /* For reading the lessons google sheet */
 function readLessons() {
-
+    console.log("Inside read lessones");
+    var url = "https://spreadsheets.google.com/feeds/list/" + lessonSpreadsheetID + "/od6/public/values?alt=json";
+    //console.log(url);
+    $.getJSON(url, function(data) {
+        if (typeof(data) == null) {
+            console.log("Data == null");
+        }
+        //console.log(data.feed.entry);
+        jsondata = data.feed.entry;
+        console.log(jsondata);
+    });
 }
